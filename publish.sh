95f8369d481c16d750731093068019195177a559
Steven Klein <steven.p.klein@gmail.com>
2020-11-24 10:56:47 -0600

    update publish endpoint

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ cd httpdocs
 git init
 git add -A
 git commit -m 'publish'
-git push -f git@github.com:steven-klein/fear-of-flying-shoot.git master:gh-pages
+git push -f git@github.com:steven-klein/iceland-2019-gallery.git master:gh-pages
 cd ../
 npm run build
